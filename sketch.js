cf289f9ab1389b4916577e6b5f75b6a0448b2b67
Chestnut1122 <jili0710@uni.sydney.edu.au>
2025-06-11 14:25:57 +1000

    create a breathing animation effect, change BG colour

--- sketch.js
+++ sketch.js
@@ -1,20 +1,18 @@
+let grids =[];
+let noiseSpeed = 0.2;
+let bgNoiseOffset = 0;
+let bgNoiseSpeed = 0.01;
+
 //draw the static Mondrian-style composition
+//Remove the 'noloop()' function.
 function setup() {
   createCanvas(windowWidth, windowHeight); //canvas size
-  background(255);
-  noLoop();  
   drawComposition();
-}
-
-//automatically called when the browser window is resized
-//resizes the canvas and redraws the composition
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-  redraw();
 }
 
 //main drawing function
 function drawComposition(){
+  grids = [];
   let y = 0;
   let lastColor = null;
 
@@ -59,24 +57,59 @@ function drawComposition(){
         }
       }
 
-      fill(chosenColor[0], chosenColor[1], chosenColor[2]);
-      stroke(0);
-      strokeWeight(8);
-      rect(x, y, w, h);
-
+      grids.push({
+        x: x,
+        y: y,
+        w: w,
+        h: h,
+        baseW: w,
+        baseH: h,
+        color: chosenColor,
+        noiseOffset: random(100)
+      });
 
       if (!(chosenColor[0] === 255 && chosenColor[1] === 255 && chosenColor[2] === 255)) {
         lastColor = chosenColor;
       }
-
       x += w;
     }
-
     y += rowHeight;
   }
+}
+
+function draw(){
+  //noise background colour
+  let n = noise(bgNoiseOffset);
+  let g = n * 255;
+  background(g, g, g);
+
+  bgNoiseOffset +=bgNoiseSpeed;
+
+  //The breathing grid
+  for(let grid of grids){
+    let scaleFactor = noise(frameCount * noiseSpeed + grid.noiseOffset) * 0.2 + 0.8;
+    let w = grid.baseW * scaleFactor;
+    let h = grid.baseH * scaleFactor;
+
+    let dx = (grid.baseW - w) / 2;
+    let dy = (grid.baseH - h) / 2;
+
+    fill(grid.color[0], grid.color[1], grid.color[2]);
+    noStroke();
+    rect(grid.x + dx, grid.y + dy, w, h);
+  }
+}
+
+//automatically called when the browser window is resized
+//resizes the canvas and redraws the composition
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  drawComposition();
 }
 
 //check whether two colors are the same
 function colorsEqual(c1, c2) {
   return c1[0] === c2[0] && c1[1] === c2[1] && c1[2] === c2[2];
 }
+
+
